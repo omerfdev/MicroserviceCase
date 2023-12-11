f9f0ca374fccb11f789535edb457538c7ac87360
omerfdev <omerfdev@gmail.com>
2023-12-11 07:19:04 +0300

    Last commit

--- SendEmailDaily/Program.cs
+++ SendEmailDaily/Program.cs
@@ -9,7 +9,6 @@ namespace SendEmailDaily
     {
         static async Task Main(string[] args)
         {
-            // Burada gerekli SMTP ve diğer bilgileri girin
             var dailyEmailSender = new DailyEmailSender (
                 smtpServer: "smtp.gmail.com",
                 smtpPort: 587,
@@ -19,17 +18,11 @@ namespace SendEmailDaily
                 recipientEmail: "omeralmali1@gmail.com",
                 subject: "Daily Database Update",
                 body: "Here are the new records added in the last day:",
-                connectionString: "Server=127.0.0.1;Port:18009;Database=AuditDB;User=root;Password=Omer1234;"
-
+                connectionString: "Server=auditdb;Port:18009;Database=AuditDB;User=root;Password=Omer1234;"
             );
 
-            // Abone olunacak sınıfı oluşturun ve olaya abone olun
             var subscriber = new DailyEmailSubscriber(dailyEmailSender);
-
-            // E-posta gönderme işlemini başlatın
             await dailyEmailSender.SendDailyEmail();
-
-
             Console.WriteLine("Press any key to exit...");
         }
     }
@@ -44,8 +37,6 @@ namespace SendEmailDaily
         private void HandleDailyEmailEvent(object sender, DailyEmailEventArgs e)
         {
             Console.WriteLine("Daily email event received. Sending email...");
-            // E-posta gönderme işlemi veya başka işlemler gerçekleştirilebilir.
-            // e.NewData içinde yeni eklenen verilere erişilebilir.
         }
     }
 }
